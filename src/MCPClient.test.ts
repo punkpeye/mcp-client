3de68e49140eed6a21b14be9906b4c004930988e
Frank <frank@punkpeye.com>
2025-03-11 01:13:32 -0600

    test: clients reads a resource that returns multiple resources

--- src/MCPClient.test.ts
+++ src/MCPClient.test.ts
@@ -507,3 +507,54 @@ test("clients reads a resource", async () => {
     },
   });
 });
+
+test("clients reads a resource that returns multiple resources", async () => {
+  await runWithTestServer({
+    server: async () => {
+      const server = new FastMCP({
+        name: "Test",
+        version: "1.0.0",
+      });
+
+      server.addResource({
+        uri: "file:///logs/app.log",
+        name: "Application Logs",
+        mimeType: "text/plain",
+        async load() {
+          return [
+            {
+              text: "a",
+            },
+            {
+              text: "b",
+            },
+          ];
+        },
+      });
+
+      return server;
+    },
+    run: async ({ client }) => {
+      expect(
+        await client.getResource({
+          uri: "file:///logs/app.log",
+        }),
+      ).toEqual({
+        contents: [
+          {
+            uri: "file:///logs/app.log",
+            name: "Application Logs",
+            text: "a",
+            mimeType: "text/plain",
+          },
+          {
+            uri: "file:///logs/app.log",
+            name: "Application Logs",
+            text: "b",
+            mimeType: "text/plain",
+          },
+        ],
+      });
+    },
+  });
+});
